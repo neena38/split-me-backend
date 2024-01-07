a5208a94eec8c5da8d68f79abb31d10475129271
= <=>
2024-01-07 18:27:01 +0530

    Created DB, added table and SPs

---
+++ Database/dbo/Stored Procedures/Profile_Insert.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[Profile_Insert]
+	@Id int OUTPUT,
+	@Name nvarchar(20),
+	@NameSet varchar(max)
+AS
+BEGIN
+	INSERT INTO dbo.Profiles ( 
+	Name, 
+	NameSet)
+	VALUES (
+	@Name, 
+	@NameSet)
+END;
+GO
---
+++ Database/dbo/Tables/Profiles.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Profiles]
+(
+	[Id] INT IDENTITY (1, 1) NOT NULL, 
+    [Name] NVARCHAR(50) NOT NULL, 
+    [NameSet] VARCHAR(MAX) NULL, 
+    CONSTRAINT [PK_Profiles] PRIMARY KEY ([Id]),
+)
